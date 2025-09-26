751cc2ce4017702a882927ce9cd67f20b8c56808
Mat-Dar <matid3221@gmail.com>
2025-09-26 11:17:58 +0200

    Update dargacz.js

--- js/zad2/dargacz.js
+++ js/zad2/dargacz.js
@@ -5,17 +5,7 @@ document.write("ostatnia modyfikacja strony".fontcolor("silver").strike().fontsi
 
 document.write(document.lastModified).fontcolor("silver"); 
 
-document.write(
-  "ostatnia modyfikacja strony"
-    .fontcolor("FFFF00")
-    .strike()
-    .fontsize(7) + "<br>"
-);
-    document.write(
-      '<p style="color:#FFFF00; font-weight:strike; font-size:7px;">' +
-        document.lastModified +
-      '</p>'
-    );
+
 
 
 
