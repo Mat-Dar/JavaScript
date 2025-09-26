9617160b489acf409103195947f1fd8921e6ff06
Mat-Dar <matid3221@gmail.com>
2025-09-26 07:37:50 +0200

    Update dargacz.js

--- js/zad2/dargacz.js
+++ js/zad2/dargacz.js
@@ -1,5 +1,6 @@
 document.write("Mój numer w dzienniku to 4 "+"<br>")
 document.write("Pierwszy wiersz to:   13     Tak Nie 6"+"<br>")
 document.write("Drugi wiersz to:          2     Nie Tak 6 "+"<br>")
-document.write("ostatnia modyfikacja strony".fontcolor("silver").bold().fontsize(6)+"<br><s>"); 
+document.write("ostatnia modyfikacja strony".fontweight(":strike:").fontcolor("silver").bold().fontsize(6)+"<br>"); 
+
 document.write(document.lastModified).fontcolor("silver"); 
